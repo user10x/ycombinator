8b0b7f061a75b59f09a63ee48862e77fa9b9ea5e
nick <nickchakra2000@gmail.com>
2025-04-10 18:18:13 -0700

    server side/client rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
+import Hello from "@/app/components/hello";
+
 export default function Home() {
   console.log("hey I am here")
   return (
-    <h1 className="text-3xl"> Hello World </h1>
+      <>
+        <h1 className="text-3xl"> Hello World </h1>
+        <Hello />
+
+      </>
+
   );
 }
