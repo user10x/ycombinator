6c19c52e23925ff35b16e6630c6cb637a8b0fe5c
nick <nickchakra2000@gmail.com>
2025-04-10 19:22:55 -0700

    dynamic routing

---
+++ app/dashboard/users/[id]/page.tsx
@@ -0,0 +1,9 @@
+
+const Page = () => {
+    return (
+        <div> User details page</div>
+    )
+}
+
+
+export default Page;
--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,13 +1,15 @@
+import Link from "next/link";
+
 const Page = ()=> {
     return (
 
         <div>
             <h1> Dashboard Users </h1>
-            <ul >
-                <li>User 1</li>
-                <li>User 2</li>
-                <li>User 3</li>
-                <li>User 4</li>
+            <ul className="mt-10">
+                <li><Link href="/dashboard/users/1"> User 1</Link></li>
+                <li><Link href="/dashboard/users/2"> User 2</Link></li>
+                <li><Link href="/dashboard/users/3"> User 3</Link></li>
+                <li><Link href="/dashboard/users/4"> User 4</Link></li>
             </ul>
         </div>
     )
