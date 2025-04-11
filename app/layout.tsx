19d305dd0c1c3bb1dafa9834610d0f6717090963
nick <nickchakra2000@gmail.com>
2025-04-10 21:15:02 -0700

    root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <h1 className="text-3xl">ROOT</h1>
         {children}
+
       </body>
     </html>
   );
