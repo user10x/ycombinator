351ce3ea5ff41be750e4970674c37f9ec5bd73ef
nick <nickchakra2000@gmail.com>
2025-04-10 21:34:17 -0700

    add grouping

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,9 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-      <h1 className="text-3xl">ROOT</h1>
         {children}
-
       </body>
     </html>
   );
