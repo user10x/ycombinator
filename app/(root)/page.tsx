578ef30b053c836d4d5b94a28200335aac74107d
nick <nickchakra2000@gmail.com>
2025-04-13 09:22:08 -0700

    add github auth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,7 @@
-import Hello from "@/app/components/hello";
-
 export default function Home() {
-  console.log("hey I am here")
-  return (
-      <>
-        <h1 className="text-3xl"> Hello World </h1>
-        <Hello />
-
-      </>
-
-  );
+    return (
+        <div>
+            <h1 className="text-2xl">HOME</h1>
+        </div>
+    );
 }
---
+++ app/app/(root)/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+      <div>
+        <h1 className="text-2xl">HOME</h1>
+      </div>
+   );
+}
