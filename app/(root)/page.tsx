91a67c6f00e6099f868ebe820d074a6763ad62c1
nick <nickchakra2000@gmail.com>
2025-04-13 20:08:15 -0700

    add tailwind css

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
     return (
         <div>
-            <h1 className="text-2xl">HOME</h1>
+            <section className="pink_container">
+                <h1 className="header">Pitch your startup <br/> Connect with Entrepreneurs </h1>
+            </section>
+            <p className="!max-w-3xl text-center">
+                Submit Ideas, Vote on Pitches, Get noticed in Virtual Competetions
+            </p>
+
+
         </div>
     );
 }
