230e5a83c347926615661e8d28c2ac6db1485055
nick <nickchakra2000@gmail.com>
2025-04-15 07:44:40 -0700

    tailwind magic

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,11 +2,12 @@ export default function Home() {
     return (
         <div>
             <section className="pink_container">
-                <h1 className="header">Pitch your startup <br/> Connect with Entrepreneurs </h1>
+                <h1 className="header">Pitch your startup <br /> Connect with Entrepreneurs </h1>
+                <p className="sub-heading !max-w-3xl">
+                    Submit Ideas, Vote on Pitches, Get noticed in Virtual Competitions
+                </p>
             </section>
-            <p className="!max-w-3xl text-center">
-                Submit Ideas, Vote on Pitches, Get noticed in Virtual Competetions
-            </p>
+
 
 
         </div>
