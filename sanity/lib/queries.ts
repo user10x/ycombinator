4d90052ec0880afa4d091914c154297832a26dbf
nick <nickchakra2000@gmail.com>
2025-04-18 09:11:39 -0700

    add sanity search param

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,8 +1,8 @@
 import { defineQuery } from "next-sanity";
 
 export const STARTUPS_QUERY =
-  defineQuery(`*[_type == "startup" && defined(slug.current) ] | order(_createdAt desc) {
-  _id,  
+  defineQuery(`*[_type == "startup" && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt desc) {
+  _id, 
   title, 
   slug,
   _createdAt,
@@ -14,6 +14,5 @@ export const STARTUPS_QUERY =
   category,
   image,
 }`);
-
 
 
